85a7d1ff24c37a74c9b9153141297e3fe9e315a4
RKirill28 <pavlov26.2008@gmail.com>
2025-10-23 20:30:46 +0700

    added crated_date column

--- db/data/order.py
+++ db/data/order.py
@@ -1,3 +1,4 @@
+import datetime
 from data.base import Base
 
 from sqlalchemy.orm import Mapped, mapped_column
@@ -7,9 +8,10 @@ class Order(Base):
     __tablename__ = 'orders'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    externel_id: Mapped[str] = mapped_column(primary_key=True)
+    external_id: Mapped[str] = mapped_column(primary_key=True)
     link: Mapped[str]
     quantity: Mapped[int]
+    created_date: Mapped[datetime.datetime]
     service_id: Mapped[int]
     service_type: Mapped[str]
     price: Mapped[int]
